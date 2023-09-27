eeb4b9bba20f2624034daa6dd708e0e47fcb7a58
Austin Gray <amgray5@mail.fhsu.edu>
2023-09-27 13:17:42 -0600

    get artist data from csv

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+#%%
 # INF601 - Advanced Programming in Python
 # Austin Gray
 # Mini Project 2
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv('artists.csv', index_col='Artist')
+df = pd.read_csv('artists.csv', index_col='Artist')
+
+artist = 'Kip Moore'
+
+artist_data   = dict(df.loc[artist])
+data_details  = list(artist_data.keys())
+artist_values = list(artist_data.values())
