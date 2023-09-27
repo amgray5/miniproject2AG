77c8d225d7730c8876dda16ffe77c199e3d4a3be
Austin Gray <amgray5@mail.fhsu.edu>
2023-09-27 13:34:48 -0600

    removes blanks from lists

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ artist = 'Kip Moore'
 artist_data   = dict(df.loc[artist])
 data_details  = list(artist_data.keys())
 artist_values = list(artist_data.values())
+
+for i in range(0, len(artist_values)):
+    if pd.isna(artist_values[i]) == True:
+        data_details.remove(data_details[i])
+        artist_values.remove(artist_values[i])
+
+# %%
